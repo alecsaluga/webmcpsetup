fe2e3d1879e5ed188b3cdc03e99e46fa287ac717
Alec Saluga <alecsaluga@MacBook-Air-3.local>
2026-02-16 20:59:25 -0800

    Remove async attribute from GA4 script (Next.js Script handles this)

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -4,7 +4,6 @@ export default function GoogleAnalytics() {
   return (
     <>
       <Script
-        async
         src="https://www.googletagmanager.com/gtag/js?id=G-5T2DN6KNVG"
         strategy="afterInteractive"
       />
